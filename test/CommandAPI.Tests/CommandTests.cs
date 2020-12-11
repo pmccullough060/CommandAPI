a943bde69980dd0b4b7a03772f4b2663ffbc9197
pmccullough060 <pmccullough060@gmail.com>
2020-12-11 19:59:16 +0000

    Started working on the tests project

---
+++ test/CommandAPI.Tests/CommandTests.cs
@@ -0,0 +1,26 @@
+using Xunit;
+using CommandAPI.Models;
+
+namespace CommandAPI.Tests
+{
+    public class CommandTests
+    {
+        [Fact]
+        public void TestName()
+        {
+        //Arrange
+            var testCommand = new Command
+            {
+                HowTo = "Do something awesome",
+                Platform = "xUnit",
+                CommandLine = "dotnet test"
+            };
+
+            //Act
+            testCommand.HowTo = "Execute Unit Tests";
+
+            //Assert
+            Assert.Equal("Execute Unit Tests", testCommand.HowTo);
+        }
+    }
+}
